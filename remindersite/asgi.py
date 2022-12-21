d0ab934cb69cd740d99128f799e65fb4d59c44e5
World <simworld415@gmail.com>
2022-12-21 16:41:08 +0900

    change project to app
    django protect => remindersite
    django app => reminder

--- remindersite/asgi.py
+++ remindersite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for reminder project.
+ASGI config for remindersite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reminder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'remindersite.settings')
 
 application = get_asgi_application()
--- remindersite/urls.py
+++ remindersite/urls.py
@@ -1,4 +1,4 @@
-"""reminder URL Configuration
+"""remindersite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
