a2505de8198dff8fa8fbcecedb996d416aee522f
World <simworld415@gmail.com>
2022-12-21 19:50:51 +0900

    implement sign in

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,6 @@ app_name = "user"
 
 urlpatterns = [
     path('', views.index),
-    path('signup/', views.signup, name='signup'),
+    path('signup', views.signup, name='signup'),
+    path('signin', auth_views.LoginView.as_view(), name='signin')
 ]
