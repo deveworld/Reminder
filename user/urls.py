48eb038ab419279566ab6a910e2d0383931338c5
World <simworld415@gmail.com>
2022-12-21 19:55:09 +0900

    set template_name as signin html

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,5 @@ app_name = "user"
 urlpatterns = [
     path('', views.index),
     path('signup', views.signup, name='signup'),
-    path('signin', auth_views.LoginView.as_view(), name='signin')
+    path('signin', auth_views.LoginView.as_view(template_name='user/signin.html'), name='signin')
 ]
