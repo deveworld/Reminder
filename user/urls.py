65bc3efe3dfe082df989014c98a35888b3c7de51
World <simworld415@gmail.com>
2022-12-21 20:04:59 +0900

    implement log out

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,6 @@ app_name = "user"
 urlpatterns = [
     path('', views.index),
     path('signup', views.signup, name='signup'),
-    path('signin', auth_views.LoginView.as_view(template_name='user/signin.html'), name='signin')
+    path('signin', auth_views.LoginView.as_view(template_name='user/signin.html'), name='signin'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
